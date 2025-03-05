dc2fa9769741590c43f3fe5a544e56b945da39a6
WojtekDev <vasquez.castro.andres@gmail.com>
2025-03-04 22:44:08 -0400

    Modificacion en la capa Modelo

---
+++ prueba/Modelo/BlancoM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba.Modelo
+{
+    public class BlancoM : ExamenBase
+    {
+        public string Muestra { get; set; }
+        public string Examen { get; set; }
+        public string Datos { get; set; }
+        public string Otros { get; set; }
+      
+        public int IdBlanco { get; set; }
+        public int IdPaciente { get; set; }
+    }
+}
