d73b6680ae00fc4ce1684ad1263dc7f3099d4137
WojtekDev <vasquez.castro.andres@gmail.com>
2025-03-14 12:32:28 -0400

    diseño

--- prueba/Vista/RegistroPaciente.Designer.cs
+++ prueba/Vista/RegistroPaciente.Designer.cs
@@ -221,7 +221,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(96, 211);
+            this.label7.Location = new System.Drawing.Point(96, 258);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(76, 25);
             this.label7.TabIndex = 13;
@@ -231,7 +231,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(97, 263);
+            this.label5.Location = new System.Drawing.Point(96, 214);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(58, 25);
             this.label5.TabIndex = 11;
@@ -270,7 +270,7 @@
             // txtMedico
             // 
             this.txtMedico.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMedico.Location = new System.Drawing.Point(194, 208);
+            this.txtMedico.Location = new System.Drawing.Point(194, 255);
             this.txtMedico.Name = "txtMedico";
             this.txtMedico.Size = new System.Drawing.Size(160, 30);
             this.txtMedico.TabIndex = 6;
@@ -278,7 +278,7 @@
             // txtEdad
             // 
             this.txtEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtEdad.Location = new System.Drawing.Point(194, 258);
+            this.txtEdad.Location = new System.Drawing.Point(193, 209);
             this.txtEdad.Name = "txtEdad";
             this.txtEdad.Size = new System.Drawing.Size(160, 30);
             this.txtEdad.TabIndex = 4;
